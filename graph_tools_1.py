61290ceb923e13f72c9be5c5839b7ab9c360664e
joe <ioanna.kolovou@gmail.com>
2016-07-08 10:09:44 +0100

    network snap

--- graph_tools_1.py
+++ graph_tools_1.py
@@ -15,6 +15,74 @@ def make_iter(my_list):
 # you may need to explode the network first
 
 
+snap_threshold = 0.0001
+
+
+def point_equality(vertex1,vertex2, snap_threshold):
+    return ((abs(vertex1[0] - vertex2[0]) < snap_threshold) and
+            (abs(vertex1[1] - vertex2[1]) < snap_threshold))
+
+
+def snap(graph):
+    # traverse nodes and group them if distance between them small
+    nodes_comb= []
+    all_nodes_comb = itertools.combinations(graph.nodes(), 2)
+    for comb in all_nodes_comb:
+        if point_equality(comb[0],comb[1],snap_threshold):
+            nodes_comb.append([comb[0],comb[1]])
+    graph_comb = to_graph(nodes_comb)
+    grouped_nodes = nx.connected_components(graph_comb)
+    node_replacement = {}
+    node_repl_comb = {}
+    for i in grouped_nodes:
+        node_replacement[list(i)[0]] = list(i)
+    for k, v in node_replacement.items():
+        for item in v:
+            if item != k:
+                node_repl_comb[item]= k
+    graph_snapped = nx.MultiGraph(graph)
+    for p0,p1,d in graph.edges(data=True):
+        new_p0 = p0
+        new_p1 = p1
+        if p0 in node_repl_comb.keys():
+            new_p0 = node_repl_comb[p0]
+        if p1 in node_repl_comb.keys():
+            new_p1 = node_repl_comb[p1]
+        if new_p0 != p0 or new_p1 != p1:
+            graph_snapped.remove_edge(p0, p1)
+            graph_snapped.add_edge(new_p0, new_p1, data=d)
+    return graph_snapped
+
+
+def keep_2_decimals(number):
+    integer_number = int(number)
+    decimal_number = str(int ((number - integer_number)*(10**(len(str(number - integer_number))-2))))
+    decimal_2 = int('0.'+decimal_number[0]+decimal_number[1])
+    return decimal_2
+
+
+def to_graph(l):
+    g = networkx.Graph()
+    for part in l:
+        # each sublist is a bunch of nodes
+        g.add_nodes_from(part)
+        # it also imlies a number of edges:
+        g.add_edges_from(to_edges(part))
+    return g
+
+
+def to_edges(l):
+    """
+        treat `l` as a Graph and returns it's edges
+        to_edges(['a','b','c','d']) -> [(a,b), (b,c),(c,d)]
+    """
+    it = iter(l)
+    last = next(it)
+    for current in it:
+        yield last, current
+        last = current
+
+
 def explode_shapefile(shp_path, expl_shp_path):
     shp_input = QgsVectorLayer(shp_path, "network", "ogr")
     processing.runalg("qgis:explodelines", shp_input, expl_shp_path)
@@ -22,8 +90,9 @@ def explode_shapefile(shp_path, expl_shp_path):
     return expl_shp
 
 
+# do you need simplify = True ??
 def shp_to_graph(shp_path):
-    graph_shp = nx.read_shp(str(shp_path))
+    graph_shp = nx.read_shp(str(shp_path), simplify=True)
     shp = QgsVectorLayer(shp_path, "network", "ogr")
     # parallel edges are excluded of the graph because read_shp does not return a multi-graph, self-loops are included
     #self_loops = [[feat.id(), feat.geometry().asPolyline()[0],feat.attributes()]for feat in shp.getFeatures() if feat.geometry().asPolyline()[0] == feat.geometry().asPolyline()[-1] ]
@@ -43,7 +112,17 @@ def shp_to_graph_2(shp_path):
     shp_edges = {i.id(): [i.geometry().asPolyline()[0],i.geometry().asPolyline()[-1]] for i in shp.getFeatures()}
     graph = nx.MultiGraph()
     for k, v in shp_edges.items():
-        graph.add_edge(tuple(v[0]), tuple(v[1]),id_1=k)
+        graph.add_edge(tuple(v[0]), tuple(v[1]), id_1=k)
+    return graph
+
+
+def shp_to_graph_3(shp_path):
+    shp = QgsVectorLayer(shp_path, "network", "ogr")
+    # shapefile should be exploded first
+    features = [x for x in shp.getFeatures()]
+    graph = nx.MultiGraph()
+    for i in features:
+        graph.add_edge(i.geometry().asPolyline()[0], i.geometry().asPolyline()[-1], attr = i.attributes())
     return graph
 
 
@@ -55,6 +134,8 @@ def shp_to_graph_2(shp_path):
 # TO DO: both of them are features
 # TO DO: construct {feature line:[feature_point, feature_point]} from shp
 
+# TO DO: add id column name as argument
+
 
 def graph_to_dual_edges(primary_graph):
     # construct a dual graph with all connections
@@ -63,8 +144,8 @@ def graph_to_dual_edges(primary_graph):
         edges = []
         if len(j) > 1:
             for k, v in j.items():
-                edges.append(v['osm_id'])
-                dual_graph_edges += [x for x in itertools.combinations(edges,2)]
+                edges.append(v[0]['attr'][0])
+                dual_graph_edges += [x for x in itertools.combinations(edges, 2)]
     return dual_graph_edges
 
 
@@ -93,9 +174,9 @@ def subgraph_for_back(shp,dual_graph ):
         "type='tertiary' or type='tertiary_link' or type= 'bridge' OR type='footway' OR type = 'living_street' OR type= 'path' OR type= 'pedestrian' OR type= 'residential' OR type= 'road' OR type= 'service' OR type= 'steps' OR type= 'track' OR type= 'unclassified' OR type='abandonded' OR type='bridleway' OR type='bus_stop' OR type='construction' OR type='elevator' OR type='proposed' OR type='raceway' OR type='rest_area'")
     osm_ids_foreground=[]
     osm_ids_background=[]
-    for elem in shp.getFeatures (QgsFeatureRequest(expr_foreground)):
+    for elem in shp.getFeatures(QgsFeatureRequest(expr_foreground)):
         osm_ids_foreground.append(elem.attribute('osm_id'))
-    for elem in shp.getFeatures (QgsFeatureRequest(expr_background)):
+    for elem in shp.getFeatures(QgsFeatureRequest(expr_background)):
         osm_ids_background.append(elem.attribute('osm_id'))
     for_sub_dual=dual_graph.subgraph(osm_ids_foreground)
     back_sub_dual = dual_graph.subgraph(osm_ids_background)
@@ -142,9 +223,5 @@ def merge_lines(dual_graph_input,dual_graph_output):
 # TO DO:
 # store features of the secondary graph that intersect the main_graph
 
-def graph_intersection (main_graph, secondary_graph):
-    # find secondary graph endpoints
-    # compare connectivity
-    pass
 
 
